987c19098fd1f04b105718c94e17d73f55bbdb4e
Mehant Kammakomati <411843@student.nitandhra.ac.in>
2020-08-01 17:48:34 +0530

    Add server code (#11)
    * Add initial folder structure
    
    * Add initial code for PASP server
    
    * Fixed API doc
    
    * Add server code

--- backend/Main.py
+++ backend/Main.py
@@ -1,1 +1,336 @@
 """ PASP enpoints defined using Flask """
+
+from flask import request, jsonify, Response
+import json
+from flask_mail import Message
+
+from utils import otp, present_date, executeSQL, validate, dumpDB, deleteDB, \
+    numberOf, maximumCount, minimumCount, numberOfEach, getFactor
+from initializer import pasp, mail, cache
+
+
+@pasp.route('/')
+@cache.cached(timeout=60)
+def check():
+    data = executeSQL('show tables', False,)
+    return Response(response=json.dumps(data), status=200)
+
+
+@pasp.route('/login')
+def login():
+    username = request.args.get('username')
+    password = request.args.get('password')
+    onepass = request.args.get('otp')
+    data = executeSQL(
+        'select uid, email from users where username="%s" and password="%s"', True, username, password)
+    print(data)
+    if data is not None:
+        uid = data[0]
+        print(uid)
+        email = data[1]
+        print(email)
+    #executeSQL('update users set otp="" where uid=%s',True, str(uid))
+    if data is not None and onepass is None:
+        key = otp()
+        print(f'update users set otp="%s" where uid=%s' % (key, str(uid)))
+        with pasp.app_context():
+            executeSQL('update users set otp="%s" where uid=%s',
+                       True, key, str(uid))
+            msg = Message(subject="OTP for PASP login",
+                          sender=pasp.config.get("MAIL_USERNAME"),
+                          recipients=[email],
+                          body='OTP: %s' % key)
+        mail.send(msg)
+        resp = Response(response='OTP sent', status=203)
+        return resp
+    elif data is not None and onepass is not None:
+        data = executeSQL(
+            'select otp from users where username="%s" and password="%s"', True, username, password)
+        print(data[0])
+        print(onepass)
+        print(uid)
+        if data[0] == onepass:
+            resp = Response(response='Welcome, "%s"' % username, status=200)
+            resp.headers["token"] = onepass
+        else:
+            resp = Response(response='Access Denied', status=401)
+        return resp
+    else:
+        return Response(response='Access Denied', status=401)
+
+
+@pasp.route('/signup')
+def signup():
+    username = request.args.get('username')
+    password = request.args.get('password')
+    onepass = request.args.get('otp')
+    emailid = request.args.get('email')
+    info = executeSQL('select uid from users where username="%s" and password="%s" and email="%s"',
+                      True, username, password, emailid)
+    if info is None or onepass is None:
+        if info is None:
+            executeSQL('insert into users (username, password, email) values \
+                 ("%s","%s","%s")', True, username, password, emailid)
+        key = executeSQL('select otp from users where username="%s" and \
+             password="%s" and email="%s"', True, username, password, emailid)
+        print(key)
+        if None not in key:
+            return Response(response='Failed', status=401)
+        key = otp()
+        with pasp.app_context():
+            msg = Message(subject="OTP for PASP login",
+                          sender=pasp.config.get("MAIL_USERNAME"),
+                          recipients=[emailid],
+                          body='OTP: %s' % key)
+        mail.send(msg)
+        executeSQL('update users set otp="%s" where username="%s" and \
+                 password="%s" and email="%s"', True, key, username, password, emailid)
+        resp = Response(response='OTP sent', status=203)
+    else:
+        key = executeSQL('select otp from users where username="%s" and \
+             password="%s" and email="%s"', True, username, password, emailid)
+        if onepass in key:
+            resp = Response(response='Success', status=200)
+        else:
+            resp = Response(response='Failed', status=401)
+    return resp
+
+
+@pasp.route('/delete')
+def delete():
+    token = request.headers["token"]
+    data = validate(token)
+    if token is None or data is None:
+        resp = Response(response='Bad Request', status=400)
+    else:
+        executeSQL('delete from input where uid=%d', True, data[0])
+        executeSQL('delete from users where uid=%d', True, data[0])
+        resp = Response(response="Deleted", status=204)
+    return resp
+
+
+@pasp.route('/forgot')
+def forgot():
+    username = request.args.get('username')
+    email = request.args.get('email')
+    data = executeSQL(
+        'select password from users where username="%s" and email="%s"', True, username, email)
+    if data is not None:
+        with pasp.app_context():
+            msg = Message(subject="Your Password has been recovered",
+                          sender=pasp.config.get("MAIL_USERNAME"),
+                          recipients=[email],
+                          body='Password: %s' % data[0])
+        mail.send(msg)
+        resp = Response(response="sent", status=200)
+    else:
+        resp = Response(response="Wrong username or email id", status=403)
+    return resp
+
+
+@pasp.route('/details')
+@cache.cached(timeout=60)
+def details():
+    token = request.headers["token"]
+    data = validate(token)
+    if token is None or data is None:
+        resp = Response(response='Bad Request', status=400)
+    else:
+        resp = Response(response=json.dumps(data), status=200)
+    return resp
+
+
+@pasp.route('/input', methods=['GET', 'POST'])
+@cache.cached(timeout=60)
+def input():
+    token = request.headers["token"]
+    data = validate(token)
+    if token is None or data is None:
+        resp = Response(response='Bad Request', status=400)
+    else:
+        if request.method == 'POST':
+            uid = data[0]
+            aid = request.args.get('aid')
+            present_d = present_date()
+            dosage = request.args.get('dosage')
+            dosage_pattern = request.args.get('dosage_pattern')
+            end_date = request.args.get('end_date')
+            executeSQL('insert into input values("%s","%s","%s","%s","%s","%s")',
+                       True, uid, aid, present_d, dosage, dosage_pattern, end_date)
+            resp = Response(response="Success", status=201)
+        elif request.method == 'GET':
+            uid = data[0]
+            dumpDB()  # create table temp with complete data
+            info = executeSQL("select * from temp where uid=%d", True, uid)
+            resp = Response(response=json.dumps(info), status=200)
+            deleteDB()
+    return resp
+
+
+@pasp.route('/gstats/numberof/<factor>')
+@cache.cached(timeout=60)
+def gstats1(factor):
+    token = request.headers["token"]
+    data = validate(token)
+    if token is None or data is None:
+        resp = Response(response='Bad Request', status=400)
+    else:
+        dumpDB()
+        info = numberOf(factor, None)
+        if info == "Failure":
+            resp = Response(response=json.dumps(info), status=404)
+        else:
+            resp = Response(response=json.dumps(info), status=200)
+        deleteDB()
+    return resp
+
+
+@pasp.route('/gstats/maxcount/<factor>')
+@cache.cached(timeout=60)
+def gstats2(factor):
+    token = request.headers["token"]
+    data = validate(token)
+    if token is None or data is None:
+        resp = Response(response='Bad Request', status=400)
+    else:
+        dumpDB()
+        info = maximumCount(factor, None)
+        if info == "Failure":
+            resp = Response(response=json.dumps(info), status=404)
+        else:
+            resp = Response(response=json.dumps(info), status=200)
+        deleteDB()
+    return resp
+
+
+@pasp.route('/gstats/mincount/<factor>')
+@cache.cached(timeout=60)
+def gstats3(factor):
+    token = request.headers["token"]
+    data = validate(token)
+    if token is None or data is None:
+        resp = Response(response='Bad Request', status=400)
+    else:
+        dumpDB()
+        info = minimumCount(factor, None)
+        if info == "Failure":
+            resp = Response(response=json.dumps(info), status=404)
+        else:
+            resp = Response(response=json.dumps(info), status=200)
+        deleteDB()
+    return resp
+
+
+@pasp.route('/gstats/numberofeach/<factor>')
+@cache.cached(timeout=60)
+def gstats4(factor):
+    token = request.headers["token"]
+    data = validate(token)
+    if token is None or data is None:
+        resp = Response(response='Bad Request', status=400)
+    else:
+        dumpDB()
+        info = numberOfEach(factor, None)
+        if info == "Failure":
+            resp = Response(response=json.dumps(info), status=404)
+        else:
+            resp = Response(response=json.dumps(info), status=200)
+        deleteDB()
+    return resp
+
+
+@pasp.route('/stats/numberof/<factor>')
+@cache.cached(timeout=60)
+def stats1(factor):
+    token = request.headers["token"]
+    data = validate(token)
+    uid = data[0]
+    if token is None or data is None:
+        resp = Response(response='Bad Request', status=400)
+    else:
+        dumpDB()
+        info = numberOf(factor, uid)
+        if info == "Failure":
+            resp = Response(response=json.dumps(info), status=404)
+        else:
+            resp = Response(response=json.dumps(info), status=200)
+        deleteDB()
+    return resp
+
+
+@pasp.route('/stats/maxcount/<factor>')
+@cache.cached(timeout=60)
+def stats2(factor):
+    token = request.headers["token"]
+    data = validate(token)
+    uid = data[0]
+    if token is None or data is None:
+        resp = Response(response='Bad Request', status=400)
+    else:
+        dumpDB()
+        info = maximumCount(factor, uid)
+        if info == "Failure":
+            resp = Response(response=json.dumps(info), status=404)
+        else:
+            resp = Response(response=json.dumps(info), status=200)
+        deleteDB()
+    return resp
+
+
+@pasp.route('/stats/mincount/<factor>')
+@cache.cached(timeout=60)
+def stats3(factor):
+    token = request.headers["token"]
+    data = validate(token)
+    uid = data[0]
+    if token is None or data is None:
+        resp = Response(response='Bad Request', status=400)
+    else:
+        dumpDB()
+        info = minimumCount(factor, uid)
+        if info == "Failure":
+            resp = Response(response=json.dumps(info), status=404)
+        else:
+            resp = Response(response=json.dumps(info), status=200)
+        deleteDB()
+    return resp
+
+
+@pasp.route('/stats/numberofeach/<factor>')
+@cache.cached(timeout=60)
+def stats4(factor):
+    token = request.headers["token"]
+    data = validate(token)
+    uid = data[0]
+    if token is None or data is None:
+        resp = Response(response='Bad Request', status=400)
+    else:
+        dumpDB()
+        info = numberOfEach(factor, uid)
+        if info == "Failure":
+            resp = Response(response=json.dumps(info), status=404)
+        else:
+            resp = Response(response=json.dumps(info), status=200)
+        deleteDB()
+    return resp
+
+
+@pasp.route('/get/<factor>')
+@cache.cached(timeout=60)
+def getF(factor):
+    token = request.headers["token"]
+    data = validate(token)
+    if token is None or data is None:
+        resp = Response(response='Bad Request', status=400)
+    else:
+
+        info = getFactor(factor)
+        if info == "Failure":
+            resp = Response(response=json.dumps(info), status=404)
+        else:
+            resp = Response(response=json.dumps(info), status=200)
+    return resp
+
+
+if __name__ == '__main__':
+    pasp.run(threaded=True)
--- backend/utils.py
+++ backend/utils.py
@@ -1,1 +1,185 @@
 """ Utility functions for PASP server """
+
+import uuid
+from flask import Flask
+from datetime import date
+from initializer import mysql
+import json
+
+
+def present_date():
+    """
+    Utility method to return present date as a string in the format "YYYY-MM-DD"  
+    """
+    return str(date.today())
+
+
+def otp():
+    """
+    Utility method to return a unique permutation of characters as a string
+    """
+    return uuid.uuid1()
+
+
+def executeSQL(sqlQuery, fetchOne=True, *params):
+    """
+    Helper method to run any SQL query
+    returns the result as a list
+    """
+
+    cur = mysql.connection.cursor()
+    try:
+        print(sqlQuery % params)
+        cur.execute(sqlQuery % params)
+        mysql.connection.commit()
+    except Exception as e:
+        print(e)
+        mysql.connection.rollback()
+        return "Failure"
+
+    result = None
+    try:
+        if fetchOne:
+            result = list(cur.fetchone())
+        else:
+            result = list(cur.fetchall())
+    except:
+        pass
+    finally:
+        cur.close()
+        return result
+
+
+def validate(token):
+    """
+    Validatation method to validate the user's login via the token
+    returns uid, username and email as a list
+    """
+    return executeSQL('select uid,username, email from users where otp="%s"', True, str(token))
+
+
+def dumpDB():
+    """
+    Helper method to create a table temp of complete PASP data
+    """
+    executeSQL("create table temp as select users.uid, users.username, users.email, input.present_date, input.dosage, input.dosage_pattern, input.end_date, \
+                antibiotics.aid, antibiotics.aname, antibiotics.risk, antibiotics.ob, antibiotics.cost, antibiotics.standard_dosing, antibiotics.effects, \
+                syndromes.sid, syndromes.sname, problems.pid, problems.pname, pathogens.pathid, pathogens.pathname,pathogens.info,pathogens.epidemiology, \
+                precautions.prec_id, precautions.prec_name from users, input,antibiotics, syndromes, problems, precautions, \
+                pathogens, need, cures, assoc where users.uid = input.uid and input.aid = antibiotics.aid and \
+                syndromes.sid = cures.sid and cures.aid = antibiotics.aid and assoc.pathid = pathogens.pathid and \
+                assoc.sid = syndromes.sid and need.pathid = pathogens.pathid and need.prec_id = precautions.prec_id \
+                and syndromes.pid = problems.pid", True)
+
+
+select users.uid, users.username, users.email, input.present_date, input.dosage, input.dosage_pattern, input.end_date, antibiotics.aid, antibiotics.aname, antibiotics.risk, antibiotics.ob, antibiotics.cost, antibiotics.standard_dosing, antibiotics.effects, syndromes.sid, syndromes.sname, problems.pid, problems.pname, pathogens.pathid, pathogens.pathname, pathogens.info, pathogens.epidemiology, precautions.prec_id, precautions.prec_name from users, input, antibiotics, syndromes, problems, precautions, pathogens, need, cures, assoc where users.uid = input.uid and input.aid = antibiotics.aid and syndromes.sid = cures.sid and cures.aid = antibiotics.aid and assoc.pathid = pathogens.pathid and assoc.sid = syndromes.sid and need.pathid = pathogens.pathid and need.prec_id = precautions.prec_id and syndromes.pid = problems.pid
+
+
+def deleteDB():
+    """
+    Helper method to drop table temp created by dumpDB
+    """
+    executeSQL("drop table temp", True)
+
+
+def numberOf(factor, uid):
+    """
+    This helper method returns number of factor by an
+    individual or globally
+    gives global stats if uid is not given
+    factor: antibiotics(aid)/pathogens(pathid)/problems(pid)/syndromes(sid)
+    """
+    if uid is None:
+        return executeSQL("select count(*) from (select distinct %s from \
+            temp) as t;", False, factor)
+    else:
+        return executeSQL("select count(*) from (select distinct %s from \
+            temp where uid=%d) as t", False, factor, uid)
+
+
+def maximumCount(factor, uid):
+    """
+    This helper method returns maximum of factor used by an
+    individual or globally
+    gives global stats if uid is not given
+    factor: antibiotics(aid)/pathogens(pathid)/problems(pid)/syndromes(sid) 
+            antibiotics(aname)/pathogens(pathname)/problems(pname)/syndromes(sname)
+    """
+    name = ""
+    if factor == "aid":
+        name = "aname"
+    if factor == "pid":
+        name = "pname"
+    if factor == "pathid":
+        name = "pathname"
+    if factor == "sid":
+        name = "sname"
+
+    if uid is None:
+        return executeSQL("select max(c), group_concat(b), group_concat(a) \
+            from (select group_concat(%s) as a, %s as b, count(*) as c from temp group by %s) as t;", False, name, factor, factor)
+    else:
+        return executeSQL("select max(c), group_concat(b), group_concat(a) \
+            from (select group_concat(%s) as a, %s as b, count(*) as c from temp where uid=%d group by %s) as t;", False, name, factor, uid, factor)
+
+
+def minimumCount(factor, uid):
+    """
+    This helper method returns minimum of factor used by an
+    individual or globally
+    gives global stats if uid is not given
+    factor: antibiotics(aid)/pathogens(pathid)/problems(pid)/syndromes(sid) 
+            antibiotics(aname)/pathogens(pathname)/problems(pname)/syndromes(sname)
+    """
+    name = ""
+    if factor == "aid":
+        name = "aname"
+    if factor == "pid":
+        name = "pname"
+    if factor == "pathid":
+        name = "pathname"
+    if factor == "sid":
+        name = "sname"
+
+    if uid is None:
+        return executeSQL("select min(c), group_concat(b), group_concat(a) \
+            from (select group_concat(%s) as a, %s as b, count(*) as c from temp group by %s) as t;", False, name, factor, factor)
+    else:
+        return executeSQL("select min(c), group_concat(b), group_concat(a) \
+            from (select group_concat(%s) as a, %s as b, count(*) as c from temp where uid=%d group by %s) as t;", False, name, factor, uid, factor)
+
+
+def numberOfEach(factor, uid):
+    """
+    This helper method returns count of each element of the factor by an
+    individual or globally
+    gives global stats if uid is not given
+    factor: antibiotics(aid)/pathogens(pathid)/problems(pid)/syndromes(sid) 
+            antibiotics(aname)/pathogens(pathname)/problems(pname)/syndromes(sname)
+    """
+    name = ""
+    if factor == "aid":
+        name = "aname"
+    if factor == "pid":
+        name = "pname"
+    if factor == "pathid":
+        name = "pathname"
+    if factor == "sid":
+        name = "sname"
+    if uid is None:
+        return executeSQL("select count(*), %s, group_concat(%s) from temp group by %s", False, factor, name, factor)
+    else:
+        return executeSQL("select count(*), %s, group_concat(%s) from temp where uid=%d group by %s", False, factor, name, uid, factor)
+
+
+def getFactor(factor):
+    """
+    This helper method returns list of complete data of a factor
+    factor: table name 
+    antibiotics/pathogens/problems/syndromes/precautions
+    """
+    if factor != "users" or factor != "need" or factor != "input" \
+            or factor != "cures" or factor != "assoc":
+        return executeSQL("select * from %s", False, factor)
+    else:
+        return "Failed"
