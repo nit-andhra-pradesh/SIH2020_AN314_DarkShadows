ac6e6d59da79cb4f215b70eb0c2998ba12633399
Mehant Kammakomati <411843@student.nitandhra.ac.in>
2020-08-02 14:28:46 +0530

    Add dashboard for PASP (#14)
    * Add initial folder structure
    
    * Add initial code for PASP server
    
    * Fixed API doc
    
    * Add server code
    
    * add dashboard

--- frontend/screens/DashComponent.js
+++ frontend/screens/DashComponent.js
@@ -1,38 +1,134 @@
 import * as React from 'react';
-import { Text, View, AsyncStorage } from 'react-native';
+import { Text, View, AsyncStorage, ScrollView, ActivityIndicator } from 'react-native';
 import { FlatList } from 'react-native-gesture-handler';
-import { Card, Icon } from 'react-native-elements';
-import { Button } from 'react-native';
+import { Icon } from 'react-native-elements';
+import { Button, StyleSheet } from 'react-native';
 import * as WebBrowser from 'expo-web-browser';
 import { Row, Col } from 'react-native-responsive-grid-system';
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph,
+  StackedBarChart
+} from "react-native-chart-kit";
+import Card from '../components/card';
+const chartConfig = {
+  backgroundGradientFrom: "#8f9eff",
+  backgroundGradientFromOpacity: 0,
+  backgroundGradientTo: "#0021f5",
+  backgroundGradientToOpacity: 0.5,
+  color: (opacity = 0.5) => `rgba(26, 255, 146, ${opacity})`,
+  strokeWidth: 2, // optional, default 3
+  barPercentage: 0.5,
+  useShadowColorFromDataset: false // optional
+};
+
+import { Dimensions, PixelRatio } from 'react-native';
+const { width, height } = Dimensions.get('window');
 
 
 class DashComponent extends React.Component {
   state = {
-    g_antibiotics: [],
-    g_pathogens: [],
-    g_problems: [],
-    g_syndromes: [],
-    u_antibiotics: [],
-    u_pathogens: [],
-    u_problems: [],
-    u_syndromes: [],
-    loading: false
+    g_antibiotics: 0,
+    antibiotics_data: [],
+
+    g_pathogens: 0,
+    pathogens_data: [],
+    g_problems: 0,
+    problems_data: [],
+    antibiotics_data: [],
+    syndromes_data: [],
+    g_syndromes: 0,
+    u_antibiotics: 0,
+    u_pathogens: 0,
+    u_problems: 0,
+    u_syndromes: 0,
+    loading: false,
+    carousel_data: [],
+    component_loading: true
   };
 
   async componentDidMount() {
+    console.log("App started")
     this.setState({
       loading: true,
     });
-    await this.get();
+    console.log("get")
+    let g = 0
+    g = await this.get()
+    this.setData()
+    // setTimeout( await this.setData(), 5000);
+    console.log("setData")
+    console.log(this.state.u_antibiotics)
+
+    // await this.setData();
     this.setState({
       loading: false,
     });
   }
+  setData = async () => {
+    while (true) {
+      let temp = {}, temp2 = {}
+      let temp3 = {}, temp4 = {}
+      let temp5 = {}, temp6 = {}
+      let temp7 = {}, temp8 = {}
 
+      let username = await AsyncStorage.getItem("username");
+      console.log("start")
+      let ta1 = [], ta2 = [], ta3 = [], ta4 = [];
+      temp["count"] = this.state.g_antibiotics;
+      temp["name"] = ": National"
+      temp["color"] = "rgb(0, 191, 255)"
+      console.log(this.state.u_antibiotics)
+      ta1.push(temp);
+      temp2["count"] = this.state.u_antibiotics;
+      temp2["name"] = ": " + username
+      temp2["color"] = "rgb(199, 3, 18)"
+      ta1.push(temp2)
+      this.setState({
+        antibiotics_data: ta1
+      })
+      temp3["count"] = this.state.g_syndromes;
+      temp3["name"] = ": National"
+      temp3["color"] = "rgb(0, 191, 255)"
+      ta2.push(temp3);
+      temp4["count"] = this.state.u_syndromes;
+      temp4["name"] = ": " + username
+      temp4["color"] = "rgb(199, 3, 18)"
+      ta2.push(temp4)
+      this.setState({
+        syndromes_data: ta2
+      })
+
+      temp5["count"] = this.state.g_pathogens;
+      temp5["name"] = ": National"
+      temp5["color"] = "rgb(0, 191, 255)"
+      ta3.push(temp5);
+      temp6["count"] = this.state.u_pathogens;
+      temp6["name"] = ": " + username
+      temp6["color"] = "rgb(199, 3, 18)"
+      ta3.push(temp6)
+      this.setState({
+        pathogens_data: ta3
+      })
+      temp7["count"] = this.state.g_problems;
+      temp7["name"] = ": National"
+      temp7["color"] = "rgb(0, 191, 255)"
+      ta4.push(temp7);
+      temp8["count"] = this.state.u_problems;
+      temp8["name"] = ": " + username
+      temp8["color"] = "rgb(199, 3, 18)"
+      ta4.push(temp8)
+      this.setState({
+        problems_data: ta4
+      })
+    }
+  }
   get = async () => {
     // g_antibiotics
-    fetch("https://pasp-api.herokuapp.com/gstats/numberofeach/aid", {
+    fetch("https://pasp-api.herokuapp.com/gstats/countof/aid", {
       method: "GET",
       headers: new Headers({
         token: await AsyncStorage.getItem("otp"),
@@ -41,7 +137,7 @@ class DashComponent extends React.Component {
       .then((response) => response.json())
       .then((json) => {
         this.setState({
-          g_antibiotics: json,
+          g_antibiotics: json[0][0],
         });
         console.log("g_antibiotics")
         console.log(this.state.g_antibiotics);
@@ -51,7 +147,7 @@ class DashComponent extends React.Component {
       });
 
     // u_antibiotics
-    fetch("https://pasp-api.herokuapp.com/stats/numberofeach/aid", {
+    fetch("https://pasp-api.herokuapp.com/stats/countof/aid", {
       method: "GET",
       headers: new Headers({
         token: await AsyncStorage.getItem("otp"),
@@ -60,7 +156,7 @@ class DashComponent extends React.Component {
       .then((response) => response.json())
       .then((json) => {
         this.setState({
-          u_antibiotics: json,
+          u_antibiotics: json[0][0],
         });
         console.log("u_antibiotics")
         console.log(this.state.u_antibiotics);
@@ -69,7 +165,7 @@ class DashComponent extends React.Component {
         console.error(error);
       });
     // g_pathogens
-    fetch("https://pasp-api.herokuapp.com/gstats/numberofeach/pathid", {
+    fetch("https://pasp-api.herokuapp.com/gstats/countof/pathid", {
       method: "GET",
       headers: new Headers({
         token: await AsyncStorage.getItem("otp"),
@@ -78,7 +174,7 @@ class DashComponent extends React.Component {
       .then((response) => response.json())
       .then((json) => {
         this.setState({
-          g_pathogens: json,
+          g_pathogens: json[0][0],
         });
         console.log("g_pathogens")
         console.log(this.state.g_pathogens);
@@ -87,7 +183,7 @@ class DashComponent extends React.Component {
         console.error(error);
       });
     // g_problems
-    fetch("https://pasp-api.herokuapp.com/gstats/numberofeach/pid", {
+    fetch("https://pasp-api.herokuapp.com/gstats/countof/pid", {
       method: "GET",
       headers: new Headers({
         token: await AsyncStorage.getItem("otp"),
@@ -96,7 +192,7 @@ class DashComponent extends React.Component {
       .then((response) => response.json())
       .then((json) => {
         this.setState({
-          g_problems: json,
+          g_problems: json[0][0],
         });
         console.log("g_problems")
         console.log(this.state.g_problems);
@@ -105,7 +201,7 @@ class DashComponent extends React.Component {
         console.error(error);
       });
     // g_syndromes
-    fetch("https://pasp-api.herokuapp.com/gstats/numberofeach/sid", {
+    fetch("https://pasp-api.herokuapp.com/gstats/countof/sid", {
       method: "GET",
       headers: new Headers({
         token: await AsyncStorage.getItem("otp"),
@@ -114,7 +210,7 @@ class DashComponent extends React.Component {
       .then((response) => response.json())
       .then((json) => {
         this.setState({
-          g_syndromes: json,
+          g_syndromes: json[0][0],
         });
         console.log("g_syndromes")
         console.log(this.state.g_syndromes);
@@ -123,7 +219,7 @@ class DashComponent extends React.Component {
         console.error(error);
       });
     // u_pathogens
-    fetch("https://pasp-api.herokuapp.com/stats/numberofeach/pathid", {
+    fetch("https://pasp-api.herokuapp.com/stats/countof/pathid", {
       method: "GET",
       headers: new Headers({
         token: await AsyncStorage.getItem("otp"),
@@ -132,7 +228,7 @@ class DashComponent extends React.Component {
       .then((response) => response.json())
       .then((json) => {
         this.setState({
-          u_pathogens: json,
+          u_pathogens: json[0][0],
         });
         console.log("u_pathogens")
         console.log(this.state.u_pathogens);
@@ -141,7 +237,7 @@ class DashComponent extends React.Component {
         console.error(error);
       });
     // u_problems
-    fetch("https://pasp-api.herokuapp.com/stats/numberofeach/pid", {
+    fetch("https://pasp-api.herokuapp.com/stats/countof/pid", {
       method: "GET",
       headers: new Headers({
         token: await AsyncStorage.getItem("otp"),
@@ -150,8 +246,9 @@ class DashComponent extends React.Component {
       .then((response) => response.json())
       .then((json) => {
         this.setState({
-          u_problems: json,
+          u_problems: json[0][0],
         });
+
         console.log("u_problems")
         console.log(this.state.u_problems);
       })
@@ -159,7 +256,7 @@ class DashComponent extends React.Component {
         console.error(error);
       });
     // u_syndromes
-    fetch("https://pasp-api.herokuapp.com/stats/numberofeach/sid", {
+    fetch("https://pasp-api.herokuapp.com/stats/countof/sid", {
       method: "GET",
       headers: new Headers({
         token: await AsyncStorage.getItem("otp"),
@@ -168,7 +265,7 @@ class DashComponent extends React.Component {
       .then((response) => response.json())
       .then((json) => {
         this.setState({
-          u_syndromes: json,
+          u_syndromes: json[0][0],
         });
         console.log("u_syndromes")
         console.log(this.state.u_syndromes);
@@ -176,40 +273,108 @@ class DashComponent extends React.Component {
       .catch((error) => {
         console.error(error);
       });
+    return 1;
   };
-
+  // _renderItem({ item, index }) {
+  //   return (
+  //     <View>
+  //       <PieChart
+  //         data={this.state.carousel_data[index]}
+  //         width={width}
+  //         height={200}
+  //         chartConfig={chartConfig}
+  //         accessor="count"
+  //         backgroundColor="transparent"
+  //         paddingLeft="15"
+  //         absolute
+  //       />        </View>
+  //   );
+  // }
   render() {
+    if (this.state.loading) {
+      return (
+        <View style={styles.loader}>
+          <ActivityIndicator size="large" color="white" />
+        </View>
+      );
+    }
     return (
       <View style={{ paddingTop: 10 }}>
-        <Row>
-          <Col xs={6} sm={6} md={4} lg={4}>
-            <Card
-              title="Globally different Antibiotic usage Count"
-              rounded>
-              <Text style={{ fontSize: 40, textAlign: "center" }}>
-                {this.state.jsonData}
-              </Text>
 
-            </Card>
+        <Text style={styles.headings}> User data / National Data </Text>
+          <Row>
+
+            <ScrollView
+              horizontal={true}
+            >
+                      <Card style={{margin: 10, overflow: 'hidden'}}>
+
+              <Text style={styles.headings}> Antibiotics:  </Text>
+              <PieChart
+                data={this.state.antibiotics_data}
+                width={width - 100}
+                height={200}
+                chartConfig={chartConfig}
+                accessor="count"
+                backgroundColor="transparent"
+                paddingLeft="15"
+                absolute
+              />
+              </Card>
+              <Card style={{margin: 10, overflow: 'hidden'}}>
+
+              <Text style={styles.headings}>Syndromes</Text>
+              <PieChart
+                data={this.state.syndromes_data}
+                width={width - 100}
+                height={200}
+                chartConfig={chartConfig}
+                accessor="count"
+                backgroundColor="transparent"
+                paddingLeft="15"
+                absolute
+              />
+              </Card>
+              <Card style={{margin: 10, overflow: 'hidden'}}>
+
+              <Text style={styles.headings}>Pathogens</Text>
+              <PieChart
+                data={this.state.pathogens_data}
+                width={width - 100}
+                height={200}
+                chartConfig={chartConfig}
+                accessor="count"
+                backgroundColor="transparent"
+                paddingLeft="15"
+                absolute
+              />
+              </Card>
+              <Card style={{margin: 10, overflow: 'hidden'}}>
+
+              <Text style={styles.headings}>Problems</Text>
+              <PieChart
+                data={this.state.problems_data}
+                width={width - 100}
+                height={200}
+                chartConfig={chartConfig}
+                accessor="count"
+                backgroundColor="transparent"
+                paddingLeft="15"
+                absolute
+              />
+              </Card>
+            </ScrollView>
+
+            {/* <Carousel
+            ref={(c) => { this._carousel = c; }}
+            data={this.state.carousel_data}
+            renderItem={this._renderItem}
+            sliderWidth={width}
+            itemWidth={width}
+          /> */}
 
 
-          </Col>
-          <Col xs={6} sm={4} md={3} lg={3}>
-            <Card
-              title="Globally different Antibiotic usage Count"
-              rounded>
-              <Text style={{ fontSize: 40, textAlign: "center" }}>
-                {this.state.jsonData}
-              </Text>
-
-            </Card>
-
-
-          </Col>
-
-
-        </Row>
-
+          </Row>
 
 
 
@@ -221,7 +386,64 @@ class DashComponent extends React.Component {
 
 }
 
-
+const styles = StyleSheet.create({
+  headings:
+  {
+    textAlign: "center",
+    fontFamily: "open-sans-bold"
+  },
+  screen: {
+    flex: 1,
+    backgroundColor: 'grey'
+  },
+  authContainer: {
+    alignContent: "center",
+    width: "90%",
+    maxWidth: 400,
+    maxHeight: 400,
+    padding: 20,
+    backgroundColor: "#424242",
+  },
+  inputView: {
+    width: "100%",
+    backgroundColor: "white",
+    borderRadius: 10,
+    height: 50,
+    marginBottom: 20,
+    justifyContent: "center",
+    padding: 20,
+  },
+  inputText: {
+    height: 50,
+    color: "black",
+  },
+  forgot: {
+    color: "white",
+    fontSize: 11,
+  },
+  loginBtn: {
+    width: "100%",
+    backgroundColor: "#d1d1d1",
+    borderRadius: 5,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    marginBottom: 10,
+  },
+  loader: {
+    flex: 1,
+    width: width,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#424242",
+  },
+  backgroundImage: {
+    width: 200,
+    height: 200,
+    paddingVertical: 100,
+  },
+});
 
 export default DashComponent
 
